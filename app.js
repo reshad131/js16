06f60656301f7f297f0de65d4cbbb9a2a3b09551
Res4ad0 <rresad019@gmail.com>
2024-04-11 13:19:30 +0400

    second commit

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ document.getElementById("addButton").addEventListener("click", function() {
     let table = document.getElementById("myTable").getElementsByTagName('tbody')[0];
     let newRow = table.insertRow(-1);
     newRow.insertCell(0).innerHTML = rowNum++;
-    newRow.insertCell(1).innerHTML = '<input type="text" class="nameInput" placeholder="Adı daxil et">';
-    newRow.insertCell(2).innerHTML = '<input type="text" class="surnameInput" placeholder="Soyadı daxil et">';
-    newRow.insertCell(3).innerHTML = '<input type="number" class="ageInput" placeholder="Yaşı daxil et">';
-    newRow.insertCell(4).innerHTML = '<button class="saveButton">Yadda saxla</button>';
+    newRow.insertCell(1).innerHTML =  '<input type="text" class="nameInput" placeholder="Adı daxil et">';
+    newRow.insertCell(2).innerHTML =  '<input type="text" class="surnameInput" placeholder="Soyadı daxil et">';
+    newRow.insertCell(3).innerHTML =  '<input type="number" class="ageInput" placeholder="Yaşı daxil et">';
+    newRow.insertCell(4).innerHTML =  '<button class="saveButton">Yadda saxla</button>';
 });
+
 
 document.getElementById("myTable").addEventListener("click", function(e) {
     if (e.target && e.target.classList.contains("saveButton")) {
